d4bab03c03b40cf348b3312387e85589c7c38ab3
keanuty <keanutythompson@gmail.com>
2025-11-21 11:02:06 -0500

    Added form messages

--- scripts.js
+++ scripts.js
@@ -1,1 +1,20 @@
-/* Add your JavaScript to this file */
+window.onload = initialize;
+
+function initialize()
+{
+    console.log("Script loaded successfully.");
+
+    const form = document.querySelector("form");
+
+    form.addEventListener("submit", function (event)
+    {
+        event.preventDefault();
+        const email = document.querySelector("input[name='email']").value;
+        const messageDiv = document.querySelector(".message");
+
+        if (!email)
+            messageDiv.textContent = "Please enter a valid email address.";
+        else
+            messageDiv.textContent = `Thank you! Your email address \`${email}\` has been added to our mailing list!`;
+    });
+}
